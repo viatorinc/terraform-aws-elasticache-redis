72b2db8312ea356be1a10943af3b16c52ff9670e
Gareth Parks <yertym@users.noreply.github.com>
2020-08-04 10:10:58 +1000

    Use the viatorinc version of null label
    This is a modified version to support testing 0.13.0

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  source     = "git::https://github.com/viatorinc/terraform-null-label.git"
   enabled    = var.enabled
   namespace  = var.namespace
   name       = var.name
